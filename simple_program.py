ace513f37605ef3fb66abba8faaec92e2b3c4473
nishh1509 <nishamishra1515@gmail.com>
2025-08-24 21:31:54 +0530

    Initial commit

---
+++ simple_program.py
@@ -0,0 +1,10 @@
+num1= float(input("enter the first number:"))
+num2= float(input("enter the second number:"))
+sum= num1+num2
+print("the sum is",sum)
+sum2= num1*num2
+print("the sum is",sum2)
+sum3= num1/num2
+print("the sum is",sum3)
+sum4= num1-num2
+print("the sum is",sum4)
